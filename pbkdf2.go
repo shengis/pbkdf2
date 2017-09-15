958a12d5e6ae27fa788429fe3b88b8dc7f8e1e66
Aurélien Fusil-Delahaye <aurelien.fusil-delahaye@shengis.org>
2017-09-15 15:55:18 +0200

    manage iter and keylen parameters

--- pbkdf2.go
+++ pbkdf2.go
@@ -1,20 +1,81 @@
 package main
 
 import (
-	"crypto/sha1"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"crypto/sha1"
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func Encrypt(content string, salt string) {
-	fmt.Printf("%x", pbkdf2.Key([]byte(content), []byte(salt), 4096, 16, sha1.New))
+type Params struct {
+	iter int
+	keylen int
+	content string
+	salt string
+}
+
+func setInt(t *int, s string, field string) error {
+	if tmp, err := strconv.Atoi(s); err != nil {
+		return errors.New(field + " must be a number")
+	} else {
+		*t = tmp
+	}
+
+	return nil
+}
+
+func (p *Params) Parse() error {
+	if len(os.Args) <= 2 {
+		return errors.New("Not enough arguments")
+	}
+
+	// Default parameters
+	p.iter = 4096
+	p.keylen = 16
+
+	for i := 1; i < len(os.Args); i++ {
+		switch os.Args[i] {
+		case "-i":
+			i++
+			if err := setInt(&p.iter, os.Args[i], "iter"); err != nil {
+				return err
+			}
+
+		case "-n":
+			i++
+			if err := setInt(&p.keylen, os.Args[i], "keylen"); err != nil {
+				return err
+			}
+
+		default:
+			if p.content == "" {
+				p.content = os.Args[i]
+			} else if p.salt == "" {
+				p.salt = os.Args[i]
+			} else {
+				return errors.New("Argument not recognized")
+			}
+		}
+	}
+
+	return nil
+}
+
+func Encrypt(content string, salt string, iter int, keylen int) {
+	fmt.Printf("%x", pbkdf2.Key([]byte(content), []byte(salt), iter, keylen, sha1.New))
 }
 
 func main() {
-	if len(os.Args) == 3 {
-		Encrypt(os.Args[1], os.Args[2])
-	} else {
-		fmt.Printf("USAGE: pbkdf2 PASSWORD SALT")
+	var params Params
+	err := params.Parse()
+
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("USAGE " + os.Args[0] + " [-i iter] [-n keylen] content salt")
+		os.Exit(1)
 	}
+
+	Encrypt(params.content, params.salt, params.iter, params.keylen)
 }
